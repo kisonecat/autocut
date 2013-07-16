d93bf45ed93083f76f05b64d607afd03e7ec2693
Jim Fowler <fowler@math.osu.edu>
2013-07-16 17:07:08 -0400

    Use XML as input format

--- autocut.rb
+++ autocut.rb
@@ -1,8 +1,18 @@
+#! /usr/bin/ruby
+# 
+# autocut.rb - automatically cut off the parts of the video where I'm not speaking
+# 
+# Jim Fowler <fowler@math.osu.edu>
+# 
+
 require 'fftw3'
 require 'tempfile'
 require 'wavefile'
 include WaveFile 
 require 'xml'
+
+################################################################
+# determine where I start and stop speaking
 
 SAMPLES_PER_BUFFER = 2048
 
@@ -10,13 +20,15 @@ def find_audio_cut_points(wave_filename)
   info = Reader.info(wave_filename)
   maximum_amplitude = 2**(info.bits_per_sample - 1)
 
-  duration_in_seconds = info.duration[:hours] * 3600 + info.duration[:minutes] * 60 + info.duration[:seconds] + info.duration[:milliseconds]/1000.0
+  duration_in_seconds = info.duration.hours * 3600 + info.duration.minutes * 60 + info.duration.seconds + info.duration.milliseconds/1000.0
 
   first_loud_moment = nil
   last_loud_moment = nil
 
   # Depending on the fundamental frequency of your voice, you may want to change this
-  frequency_window = 50..150   
+  frequency_window = 50..150
+
+  # my chair tended to squeak in this range
   anti_frequency_window = 850..900
   maximum = nil
 
@@ -76,57 +88,62 @@ def find_audio_cut_points(wave_filename)
     
   end
 
-  return [first_loud_moment, last_loud_moment]
+  return first_loud_moment..last_loud_moment
 end
 
 def find_video_cut_points(movie_filename)
   filename1 = Tempfile.new(['movie-audio-one','.wav'])
   filename2 = Tempfile.new(['movie-audio-two','.wav'])
   system( "ffmpeg -y -i #{movie_filename} -vn -ac 1 -ar 44100 -f wav #{filename1.path} 2>/dev/null" )
+  # I am not sure why FFTW is happier if I run the audio through sox
   system( "sox #{filename1.path} #{filename2.path}" )
   return find_audio_cut_points(filename2.path)
 end
 
-movie_list = []
+################################################################
+# load videos from the XML file
 
-lines = File.open( ARGV[0] ).readlines
-for line in lines
-  line = line.gsub( / *#.*/, '' )
-  movie_list << line.strip if line.match( /[A-z0-9]/ )
+videos = []
+
+input_parser = XML::Parser.file( ARGV[0] )
+doc = input_parser.parse
+for video in doc.find('/movie/video')
+  videos << video
 end
 
-cuts = Hash.new
+for video in videos
+  if video['in'].nil? or video['out'].nil?
+    puts "Listening to #{video['src']}..."
+    cuts = find_video_cut_points( video['src'] )
+    # attributes must be strings, not floats, unfortunately
+    video['in'] ||= (cuts.first - 0.30).to_s
+    video['out'] ||= (cuts.last + 0.35).to_s
+    puts video
+  end
 
-for mts in movie_list
-  mp4 = mts.gsub( /\.mts$/, '.mp4' )
-
-  if not mts.match( '-title.mp4' )
-    puts "Listening to #{mts}..."
-    cuts[mts] = find_video_cut_points( mts )
-    puts "  cut in at #{cuts[mts][0]} and out at #{cuts[mts][1]}"
-  else
-    cuts[mts] = [0,5]
+  if video['id'].nil?
+    video['id'] = video['src'].gsub( /\..*/, '' )
   end
 end
 
-#movie_list.collect!{ |x| x.gsub( /\.mts$/, '.mp4' ) }
-
 f = File.open("editlist.xml","w")
+
 f.puts "<mlt>"
 
-for movie in movie_list
-  producer = movie.gsub( /\..*/, '' )
-
-  f.puts "  <producer id=\"#{producer}\">"
-  f.puts "    <property name=\"resource\">#{movie}</property>"
+for video in videos
+  f.puts "  <producer id=\"#{video['id']}-source\">"
+  f.puts "    <property name=\"resource\">#{video['src']}</property>"
   f.puts "  </producer>"
 
-  if movie.match( /-flip/ )
 f.puts <<EOF
-    <tractor id="#{producer}-filtered">
+    <tractor id="#{video['id']}">
        <multitrack>
-         <track producer="#{producer}"/>
+         <track producer="#{video['id']}-source"/>
        </multitrack>
+EOF
+
+  unless video['flip'].nil?
+    f.puts <<EOF
        <filter>
          <property name="track">0</property>
          <property name="mirror">flip</property>
@@ -137,58 +154,76 @@ f.puts <<EOF
          <property name="mirror">flop</property>
          <property name="mlt_service">mirror</property>
        </filter>
-     </tractor>
 EOF
   end
+  f.puts "    </tractor>"
 end
 
+################################################################
+# add producers for extra audio tracks
+
+for video in videos
+  unless video['audio'].nil?
 f.puts <<EOF
-  <producer id="theme">
-    <property name="resource">music.wav</property>
+  <producer id="#{video['id']}-audio">
+    <property name="resource">#{video['audio']}</property>
     <property name="normalise"></property>
   </producer>
 EOF
-
-f.puts "  <playlist id=\"main_playlist\">"
-
-for movie in movie_list
-  producer = movie.gsub( /\..*/, '' )
-  if producer.match( /-flip/ )
-    producer = producer + '-filtered'
   end
-
-  cut_in = 0
-  cut_out = 30*5
-
-  if not movie.match( '-title.mp4' )
-    melted = `melt #{movie} -consumer xml`
-    parser = XML::Parser.string(melted)
-    doc = parser.parse
-    frame_rate_num = doc.find('profile')[0]['frame_rate_num'].to_i
-    frame_rate_den = doc.find('profile')[0]['frame_rate_den'].to_i
-    frame_rate_num = 90000
-    frame_rate_den = 1501
-    frame_rate = frame_rate_num.to_f / frame_rate_den.to_f
-    frame_rate = 30.0
-
-    cut_in = ((cuts[movie][0] - 0.30) * frame_rate).floor
-    cut_out = ((cuts[movie][1] + 0.35) * frame_rate).ceil
-  end
-  
-  f.puts "    <entry producer=\"#{producer}\" in=\"#{cut_in}\" out=\"#{cut_out}\"/>"
 end
+
+################################################################
+# write main playlist, storing starting frames along the way
+
+playlist = "main-playlist"
+f.puts "  <playlist id=\"#{playlist}\">"
+
+scene_start = 0
+
+for video in videos
+
+  melted = `melt #{video['src']} -consumer xml`
+  parser = XML::Parser.string(melted)
+  doc = parser.parse
+  frame_rate_num = doc.find('profile')[0]['frame_rate_num'].to_i
+  frame_rate_den = doc.find('profile')[0]['frame_rate_den'].to_i
+  frame_rate_num = 90000
+  frame_rate_den = 1501
+  frame_rate = frame_rate_num.to_f / frame_rate_den.to_f
+  frame_rate = 30.0
+
+  cut_in = ((video['in'].to_f) * frame_rate).floor
+  cut_out = ((video['out'].to_f) * frame_rate).ceil
+
+  video['start'] = scene_start.to_s
+  scene_length = cut_out - cut_in
+  scene_start = scene_start + scene_length
+
+  f.puts "    <entry producer=\"#{video['id']}\" in=\"#{cut_in}\" out=\"#{cut_out}\"/>"
+end
+
 f.puts "  </playlist>"
 
+total_frames = scene_start
+
+################################################################
+# make playlists to mix together the extra audio tracks
+
+for video in videos
+  unless video['audio'].nil?
 f.puts <<EOF
-  <playlist id="theme_playlist">
-    <entry producer="theme"/>
+  <playlist id="#{video['id']}-audio-playlist">
+    <blank length="#{video['start']}"/>
+    <entry producer="#{video['id']}-audio" out="#{total_frames - video['start'].to_i}"/>
   </playlist>
-  <tractor id="tractor">
+
+  <tractor id="#{playlist}-mix">
     <multitrack>
-      <track producer="main_playlist"/>
-      <track producer="theme_playlist"/>
+      <track producer="#{playlist}"/>
+      <track producer="#{video['id']}-audio-playlist"/>
     </multitrack>
-    <transition id="transition0" out="9999">
+    <transition id="transition-#{playlist}" out="#{total_frames}">
       <property name="a_track">0</property>
       <property name="b_track">1</property>
       <property name="mlt_type">transition</property>
@@ -197,7 +232,10 @@ f.puts <<EOF
   </tractor>
 EOF
 
+    playlist = playlist + "-mix"
+  end
+end
+
 f.puts "</mlt>"
 f.close
 
-# http://renomath.org/video/linux/interlace/
